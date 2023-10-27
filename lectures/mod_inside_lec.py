1ffc39d624b9fc804f9aace20f90c0f31facef89
Kelly Lai <kellylai@Kellys-MacBook-Air.local>
2023-10-27 12:05:54 +1100

    Initial commit

---
+++ lectures/mod_inside_lec.py
@@ -0,0 +1,7 @@
+""" mod_inside_lec.py
+
+A module inside the `lectures` package
+"""
+
+print("You can see me!")
+
---
+++ toolkit_config.py
@@ -0,0 +1,10 @@
+""" toolkit_config.py
+
+Project configuration file
+"""
+import os
+
+
+PRJDIR = '/Users/kellylai/PycharmProjects/toolkit'
+DATADIR = os.path.join(PRJDIR, 'data')
+
